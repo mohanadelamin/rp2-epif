06c350fb742373e8cf2ac1fb5851245225d950bb
Mohanad Elamin <mohanad.elamin@os3.nl>
2021-06-08 10:47:53 +0100

    Move to home

--- start_all_services.sh
+++ start_all_services.sh
@@ -1,4 +1,4 @@
-kubectl apply  -f epi-client-configmap.yaml
+kubectl apply -f epi-client-configmap.yaml
 kubectl apply -f epi-proxy.yaml
 kubectl apply -f epi-client.yaml
 kubectl apply -f epi-server.yaml
--- stop_all_services.sh
+++ stop_all_services.sh
@@ -1,4 +1,4 @@
-kubectl delete  -f epi-client-configmap.yaml
+kubectl delete -f epi-client-configmap.yaml
 kubectl delete -f epi-proxy.yaml
 kubectl delete -f epi-client.yaml
 kubectl delete -f epi-server.yaml
