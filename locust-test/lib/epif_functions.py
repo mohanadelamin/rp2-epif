cc41b21fd89666e02a426b754f72a0dfcd287571
Mohanad Elamin <mohanad.elamin@os3.nl>
2021-06-16 10:19:04 +0100

    Modify bf logic

--- locust-test/lib/epif_functions.py
+++ locust-test/lib/epif_functions.py
@@ -6,22 +6,22 @@ import random
 def choose_random_page():
     pages = [
         '/get',
-        '/response-headers?freeform=helloworld!',
-        '/headers',
-        '/ip',
-        #'/user-agent'
-        '/deflate',
-        '/deny',
-        '/encoding/utf8',
-        '/gzip',
+        # '/response-headers?freeform=helloworld!',
+        # '/headers',
+        # '/ip',
+        # #'/user-agent'
+        # '/deflate',
+        # '/deny',
+        # '/encoding/utf8',
+        # '/gzip',
         '/html',
         '/json',
-        '/xml',
-        '/uuid',
-        '/drip?duration=2&numbytes=10&code=200&delay=2',
+        # '/xml',
+        # '/uuid',
+        # '/drip?duration=2&numbytes=10&code=200&delay=2',
         #'/delay/' + str(random.randrange(10)),
         #'/bytes/' + str(random.randrange(100)),
-        '/cookies',
+        # '/cookies',
         '/anything'
     ]
 
--- locust-test/main.py
+++ locust-test/main.py
@@ -8,7 +8,7 @@ default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4
 
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 2)
+    #wait_time = between(1, 2)
 
     @task(1)
     def get_index(self):
