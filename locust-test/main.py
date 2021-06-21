e257b9469c1e181368f762d043038a91ff74509c
Mohanad Elamin <mohanad.elamin@os3.nl>
2021-06-21 20:10:06 +0100

    Add new test file

--- locust-test/main.py
+++ locust-test/main.py
@@ -6,7 +6,7 @@ from locust import User, task, between
 from lib.epif_functions import choose_random_page
 
 
-default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+default_headers = {'Connection': 'close', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
 
 class WebsiteUser(User):
@@ -19,11 +19,3 @@ class WebsiteUser(User):
     @task(3)
     def get_random_page(self):
         answer = requests.get(os.environ.get("LOCUST_HOST") + choose_random_page(), headers=default_headers)
-
-    # @task(1)
-    # def get_index(self):
-    #     self.client.get("/", headers=default_headers)
-
-    # @task(3)
-    # def get_random_page(self):
-    #     self.client.get(choose_random_page(), headers=default_headers)
