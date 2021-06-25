458f2a97e70d02f5bd93abb7bb3be5d3324c698d
Mohanad Elamin <mohanad.elamin@gmail.com>
2021-06-25 09:17:48 +0200

    Add resutls for long sessions

--- locust-test/main.py
+++ locust-test/main.py
@@ -3,8 +3,8 @@ from locust import HttpUser, task, between, constant
 from lib.epif_functions import choose_random_page
 
 
-default_headers = {'Connection': 'close','User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
-
+#default_headers = {'Connection': 'close','User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
 class WebsiteUser(HttpUser):
     # wait_time = between(1, 2)
